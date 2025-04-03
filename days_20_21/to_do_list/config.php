d427e458a5f4670cb88b2b8dcb8a51a51709e6c8
craignyu <kapp@cs.nyu.edu>
2025-04-03 15:11:04 -0400

    Days 20/21

---
+++ days_20_21/to_do_list/config.php
@@ -0,0 +1,5 @@
+<?php
+  // update this with your own file path!
+  // in VS Code you can do this by right-clicking on the directory and selecting 'copy path'
+  //$path = '/path/to/your/database/folder/should/go/here';
+?>
