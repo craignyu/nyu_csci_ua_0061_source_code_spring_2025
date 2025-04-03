f1a9c54e01eb7511eec1cfbaa628b3019e2b1e7c
craignyu <kapp@cs.nyu.edu>
2025-04-03 15:21:30 -0400

    Days 20/21

--- days_20_21/to_do_list/config.php
+++ days_20_21/to_do_list/config.php
@@ -1,5 +1,6 @@
 <?php
-  // update this with your own file path!
-  // in VS Code you can do this by right-clicking on the directory and selecting 'copy path'
-  //$path = '/path/to/your/database/folder/should/go/here';
+  // update this variable with your own file path!
+  // - in VS Code, you can do this by right-clicking on the directory and selecting 'copy path'
+  // - with a terminal connection, you can navigate to your database folder and issue the 'pwd' command
+  $path = '/path/to/your/database/folder/should/go/here';
 ?>
